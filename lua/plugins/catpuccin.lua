d85fb7f1e788fe0578592e69b0b076814999dbad
Yousef Alaraj <yousefelaraj@gmail.com>
2026-08-09 13:24:30 +0300

    backup

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -5,12 +5,71 @@ return {
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				background = { -- :h background
-					dark = "mocha",
+				flavour = "mocha", -- Changed to match the preview image
+				transparent_background = false,
+				float = {
+					transparent = false,
+					solid = false,
+				},
+				term_colors = false,
+				dim_inactive = {
+					enabled = false,
+					shade = "dark",
+					percentage = 0.15,
+				},
+				no_italic = false,
+				no_bold = false,
+				no_underline = false,
+				styles = {
+					comments = { "italic" }, -- Kept italic (matches the Lua file in the preview)
+					conditionals = {}, -- Cleared to match the C++ file in the preview
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				lsp_styles = {
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+						ok = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+						ok = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+				color_overrides = {},
+				custom_highlights = {},
+				auto_integrations = true,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					notify = false,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+					rainbow_delimiters = true,
 				},
 			})
-			vim.cmd.colorscheme("catppuccin-nvim")
+			-- Make sure the theme actually loads
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
--- lua/void/set.lua
+++ lua/void/set.lua
@@ -46,3 +46,13 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.breakindent = true -- If a bullet point wraps, indent the next line to match
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
